c210fadbdc29590febe9acf8d7633a866654ee93
Admin <WIPES.6098@GMAIL.COM>
2023-10-31 11:27:23 +0200

    Initial commit

---
+++ app/src/main/java/Draz/afinal/Sign_IN.java
@@ -0,0 +1,14 @@
+package Draz.afinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sign_IN extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
