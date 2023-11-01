b37345a522cc5f27b3f557bff6ae512d951ad0d9
Admin <WIPES.6098@GMAIL.COM>
2023-11-01 11:36:16 +0200

    k

--- app/src/main/java/Draz/afinal/Splash_Screen.java
+++ app/src/main/java/Draz/afinal/Splash_Screen.java
@@ -2,7 +2,11 @@ package Draz.afinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 public class Splash_Screen extends AppCompatActivity {
 
@@ -10,5 +14,36 @@ public class Splash_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        //start next activity (screen) automatically  after period of time
+        Handler h= new Handler() {
+            @Override
+            public void publish(LogRecord record) {
+
+            }
+
+            @Override
+            public void flush() {
+
+            }
+
+            @Override
+            public void close() throws SecurityException {
+
+            }
+        };
+        Runnable r=new Runnable() {
+            @Override
+            public void run()
+            {
+                //to open new activity from current to next activity
+                Intent i= new Intent(Splash_Screen.this,   Sign_IN.class);
+                startActivity(i);
+                //to close current activity
+                finish();
+            }
+        };
+        h.postDelayed(r,3000);
+
+
     }
 }
