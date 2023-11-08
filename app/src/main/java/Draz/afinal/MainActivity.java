b667cd16a1816f2f7ad8fbe2e0b79130f1448df2
Admin <WIPES.6098@GMAIL.COM>
2023-11-08 08:53:46 +0200

    k

--- app/src/main/java/Draz/afinal/MainActivity.java
+++ app/src/main/java/Draz/afinal/MainActivity.java
@@ -3,12 +3,34 @@ package Draz.afinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private editTExt
+    private TextInputLayout etText ;
+    private TextInputLayout
+    private TextInputLayout
+    private TextInputLayout
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("draz","onCreate");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+    public void onKlickGo (View v)
+    {
+
+
+
+
     }
 }
