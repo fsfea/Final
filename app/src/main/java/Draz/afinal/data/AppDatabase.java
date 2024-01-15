9911b4bfb11da10cd260696f9563626b131c88e7
Admin <WIPES.6098@GMAIL.COM>
2024-01-15 09:41:42 +0200

    k

--- app/src/main/java/Draz/afinal/data/AppDatabase.java
+++ app/src/main/java/Draz/afinal/data/AppDatabase.java
@@ -10,74 +10,68 @@ import androidx.room.RoomDatabase;
 import Draz.afinal.data.MyMessage.MyMessages;
 import Draz.afinal.data.usersTable.MyUser;
 import Draz.afinal.data.usersTable.MyuserQuery;
-import drz.example.draz27.data.SubjectTable.MySubject;
-import drz.example.draz27.data.SubjectTable.MySubjectQuery;
-import drz.example.draz27.data.mytasksTable.MyTasks;
-import drz.example.draz27.data.mytasksTable.MyTasksQuery;
-import drz.example.draz27.data.usersTable.MyUser;
-import drz.example.draz27.data.usersTable.MyuserQuery;
+
 /*
 تعريف الجداول ورقم الاصدار
-version
-عند تغيير اي شيء يخص الجداول او دوال علينا تغيير رقم الاصدار
-ليتم بناء قاعدة البيانات من جديد
+        version
+        عند تغيير اي شيء يخص الجداول او دوال علينا تغيير رقم الاصدار
+        ليتم بناء قاعدة البيانات من جديد
+        */
+
+
+@Database(entities = {MyUser.class,  MyMessages.class},version = 6)
+/**
+ * الفئة المسؤولة عن بناء قاعدة البيانات بكل جداولها
+ * وتوفر لنا كائن للتعمل مع قاعدة البيانات
  */
 
 
 
-        @Database(entities = {MyUser.class,  MyMessages.class},version = 6)
+public abstract class AppDatabase extends RoomDatabase
+{
         /**
-         * الفئة المسؤولة عن بناء قاعدة البيانات بكل جداولها
-         * وتوفر لنا كائن للتعمل مع قاعدة البيانات
+         * كائن للتعامل مع قاعدة البيانات
+         */
+        private static AppDatabase db;
+
+        /**
+         * يعيد كائن لعمليات جدول المستعملين
+         * @return
+         */
+
+        public abstract MyuserQuery getMyUserQuery();
+
+        /**
+         *  يعيد كائن لعمليات جدول الموضيع
+         * @return
          */
 
 
+        public abstract MySubjectQuery getMySubjectQuery();
 
-public abstract class AppDatabase extends RoomDatabase
-        {
-                /**
-                 * كائن للتعامل مع قاعدة البيانات
-                 */
-                private static AppDatabase db;
+        /**
+         *  يعيد كائن لعمليات جدول المهمات
+         * @return
+         */
 
-                /**
-                 * يعيد كائن لعمليات جدول المستعملين
-                 * @return
-                 */
+        public abstract MyTasksQuery getMyTaskQuery();
 
-                public abstract MyuserQuery getMyUserQuery();
+        /**
+         * بناء قاعدة البيانات واعادة كائن يؤشر عليها
+         * @param context
+         * @return
+         */
 
-                /**
-                 *  يعيد كائن لعمليات جدول الموضيع
-                 * @return
-                 */
-
-
-                public abstract MySubjectQuery getMySubjectQuery();
-
-                /**
-                 *  يعيد كائن لعمليات جدول المهمات
-                 * @return
-                 */
-
-                public abstract MyTasksQuery getMyTaskQuery();
-
-                /**
-                 * بناء قاعدة البيانات واعادة كائن يؤشر عليها
-                 * @param context
-                 * @return
-                 */
-
-                public static AppDatabase getDB(Context context) {
-                        if (db == null) {
-                                db = Room.databaseBuilder(context,
-                                                AppDatabase.class, "database-name")
-                                        .fallbackToDestructiveMigration()
-                                        .allowMainThreadQueries()
-                                        .build();
-                        }
-                        return db;
-
-
+        public static AppDatabase getDB(Context context) {
+                if (db == null) {
+                        db = Room.databaseBuilder(context,
+                                        AppDatabase.class, "database-name")
+                                .fallbackToDestructiveMigration()
+                                .allowMainThreadQueries()
+                                .build();
                 }
+                return db;
+
+
         }
+}
